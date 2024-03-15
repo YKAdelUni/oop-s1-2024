54b345b8b9c553fd13eb49460c20574e362d49c7
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2024-03-15 13:37:47 +1030

    1-2

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
 
-extern int is_identity(int array[10][10]);
-
 using namespace std;
 
+extern void modifyArray(double* array, int size, double value);
+
 int main(void){
-    int array[10][10] = {
-        {1,0,0,0,0,0,0,0,0,0},
-        {0,1,0,0,0,0,0,0,0,0},
-        {0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,0,0},
-        {0,0,0,0,0,1,0,0,0,0},
-        {0,0,0,0,0,0,1,0,0,0},
-        {0,0,0,0,0,0,0,1,0,0},
-        {0,0,0,0,0,0,0,0,1,0},
-        {0,0,0,0,0,0,0,0,0,1}
-    };
-
-    if (is_identity(array) == 1){
-        cout << "This array is an identity matrix" << endl;
-    } else {
-        cout << "This array is NOT an identity matrix" << endl;
+    double array[5] = {1,2,3,4,5};
+    int size = 5;
+    modifyArray(array,size,4);
+    for (int i = 0; i < size; i++){
+        cout << array[i] << " ";
     }
-
+    cout << endl;
     return 0;
 }
