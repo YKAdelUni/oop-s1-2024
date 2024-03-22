fc02bba188719e4dece2febd3e14d6348670b1de
YKAdelUni <140472101+YKAdelUni@users.noreply.github.com>
2024-03-22 20:01:39 +1030

    sub3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,56 +1,12 @@
 #include <iostream>
+#include "Person.h"
 
-using namespace std;
-
-void count_digits(int array[4][4]){
-    int zeros = 0;
-    int ones = 0;
-    int twos = 0;
-    int threes = 0;
-    int fours = 0;
-    int fives = 0;
-    int sixes = 0;
-    int sevens = 0;
-    int eights = 0;
-    int nines = 0;
-
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            switch (array[i][j]){
-                case 0:
-                    zeros++;
-                    break;
-                case 1:
-                    ones++;
-                    break;
-                case 2:
-                    twos++;
-                    break;
-                case 3:
-                    threes++;
-                    break;
-                case 4:
-                    fours++;
-                    break;
-                case 5:
-                    fives++;
-                    break;
-                case 6:
-                    sixes++;
-                    break;
-                case 7:
-                    sevens++;
-                    break;
-                case 8:
-                    eights++;
-                    break;
-                case 9:
-                    nines++;
-                    break;
-                
-            }
-        }
-    }
-
-    cout << "0:" << zeros << ";1:" << ones << ";2:" << twos << ";3:" << threes << ";4:" << fours << ";5:" << fives << ";6:" << sixes << ";7:" << sevens << ";8:" << eights << ";9:" << nines << endl;
+PersonList deepCopyPersonList(PersonList pl) {
+    PersonList new_pl;
+    new_pl.people = new Person[pl.numPeople];
+    new_pl.numPeople = pl.numPeople;
+    for (int i = 0; i < pl.numPeople; ++i) {
+        new_pl.people[i].name = pl.people[i].name;
+        new_pl.people[i].age = pl.people[i].age;
+    return new_pl;
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,16 +1,23 @@
+// main-1-3.cpp
 #include <iostream>
+#include "Person.h"
 
-extern void count_digits(int array[4][4]);
+extern PersonList deepCopyPersonList(PersonList pl);
+
+using namespace std;
 
 int main(void){
-    int array[4][4] = {
-        {1,2,3,4},
-        {2,3,4,5},
-        {3,4,5,6},
-        {4,5,6,7}
-    };
-
-    count_digits(array);
-
-    return 0;
+    PersonList original_list;
+    original_list.numPeople = 2;
+    original_list.people = new Person[original_list.numPeople];
+    original_list.people[0].name = "Alice";
+    original_list.people[0].age = 35;
+    original_list.people[1].name = "Bob";
+    original_list.people[1].age = 42;
+    PersonList copied_list = deepCopyPersonList(original_list);
+    for (int i = 0; i < copied_list.numPeople; ++i) {
+        cout << "Name: " << copied_list.people[i].name << ", Age: " << copied_list.people[i].age << endl;
+    }
+    delete [] original_list.people;
+    delete [] copied_list.people;
 }
